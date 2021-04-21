ea98047b702a58f2bb067660935466f260e60518
shadow-prince <59025976+shadow-prince@users.noreply.github.com>
2021-04-21 12:19:06 +0530

    🎈 Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { CustomerComponent } from './customer/customer.component';
+import { DriverComponent } from './driver/driver.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    CustomerComponent,
+    DriverComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
